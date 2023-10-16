fa4a4e586fe153b2feafd37a2a01466f538a1382
Tadeu Rangel <trangel.dev@gmail.com>
2023-10-15 20:12:22 -0700

    refact: complete CRUD on item repository.

--- Repositories/ItemRepository.cs
+++ Repositories/ItemRepository.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using aspnet_core_base_api.Exceptions;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 public class ItemRepository
@@ -11,9 +14,23 @@ public class ItemRepository
 
     public async Task<IEnumerable<Item>> GetItemsAsync()
     {
-        var items = from i in _context.Items select i;
+        var col = from i in _context.Items select i;
 
-        return await items.ToListAsync();
+        return await col.ToListAsync();
+    }
+
+    public async Task<Item> GetItemByIdAsync(int id)
+    {
+        var queryResult = from i in _context.Items where i.Id == id select i;
+
+        var entity = await queryResult.FirstOrDefaultAsync();
+
+        if (entity == null)
+        {
+            throw new NotFoundException($"Entity with id {id} not found");
+        }
+
+        return entity;
     }
 
     public async Task<Item> CreateItemAsync(Item item)
@@ -21,5 +38,40 @@ public class ItemRepository
         _context.Add(item);
         await _context.SaveChangesAsync();
         return item;
+    }
+
+    public async Task<Item> UpdateItemAsync(Item item)
+    {
+        try
+        {
+            _context.Update(item);
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!ItemExists(item.Id))
+            {
+                throw new NotFoundException($"Entity with id {item.Id} not found");
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return item;
+    }
+
+    public async Task<Item> DeleteItemAsync(int id)
+    {
+        var entity = await GetItemByIdAsync(id);
+        _context.Remove(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    private bool ItemExists(int id)
+    {
+        return (_context.Items?.Any(e => e.Id == id)).GetValueOrDefault();
     }
 }
