087b0fbdc4ac1d0a1ae0b6c2ce80129f46164188
Tadeu Rangel <trangel.dev@gmail.com>
2023-10-15 20:21:17 -0700

    chore: complete CRUD on Items controller.

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -19,9 +19,32 @@ public class ItemsController : ControllerBase
         return await _repository.GetItemsAsync();
     }
 
+    [HttpGet("{id}")]
+    public async Task<Item> GetItemByIdAsync(int id)
+    {
+        return await _repository.GetItemByIdAsync(id);
+    }
+
     [HttpPost]
     public async Task<Item> CreateItemAsync(Item item)
     {
         return await _repository.CreateItemAsync(item);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<Item> UpdateItemAsync(int id, Item item)
+    {
+        if (id != item.Id)
+        {
+            throw new BadHttpRequestException("Id mismatch");
+        }
+
+        return await _repository.UpdateItemAsync(item);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task DeleteItemAsync(int id)
+    {
+        await _repository.DeleteItemAsync(id);
     }
 }
