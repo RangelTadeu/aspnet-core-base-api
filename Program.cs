8d211a45f1b6cdcde797ea439136fb3a72ee85f0
Tadeu Rangel <trangel.dev@gmail.com>
2023-10-15 19:14:32 -0700

    feat: add item repository to DI container

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<BaseDbContext>(
         )
 );
 
+builder.Services.AddScoped<ItemRepository>();
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
